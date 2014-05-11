ae23f077a95684127f631c36eb3d8c8972afe8fb
yunkaiyueming <819664855@qq.com>
2014-05-11 13:26:24 +0800

    删除测试模板
    s

--- YmPHP/lib/Template/Compile/test.tpl.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-</head>
-<body>
-这是模板文件<br>
-显示模板引擎分配过来的变量数据：<font color='blue'><br><?php echo $this->tplVars["test"];?></font>
-</body>
-
